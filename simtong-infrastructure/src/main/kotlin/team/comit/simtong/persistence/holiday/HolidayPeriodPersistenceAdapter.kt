7324fa290e99ff32cbea9d2d33e31756d01a3f84
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-01-05 00:24:27 +0900

    add: (#9) 휴무표 작성 기간 Persistence Adapter

---
+++ simtong-infrastructure/src/main/kotlin/team/comit/simtong/persistence/holiday/HolidayPeriodPersistenceAdapter.kt
@@ -0,0 +1,30 @@
+package team.comit.simtong.persistence.holiday
+
+import org.springframework.stereotype.Component
+import team.comit.simtong.domain.holiday.model.HolidayPeriod
+import team.comit.simtong.domain.holiday.outbound.port.QueryHolidayPeriodPort
+import team.comit.simtong.global.extension.CollectionExtensionUtils.mapNonNull
+import team.comit.simtong.persistence.holiday.mapper.HolidayPeriodMapper
+import team.comit.simtong.persistence.holiday.repository.HolidayPeriodJpaRepository
+import java.time.LocalDate
+
+/**
+ *
+ * 휴무표 작성 기간의 영속성을 관리하는 HolidayPeriodPersistenceAdapter
+ *
+ * @author Chokyunghyeon
+ * @date 2023/01/03
+ * @version 1.0.0
+ **/
+@Component
+class HolidayPeriodPersistenceAdapter(
+    private val holidayPeriodJpaRepository: HolidayPeriodJpaRepository,
+    private val holidayPeriodMapper: HolidayPeriodMapper
+) : QueryHolidayPeriodPort {
+
+    override fun queryHolidayPeriodsByEndAt(endAt: LocalDate): List<HolidayPeriod> {
+        return holidayPeriodJpaRepository.queryHolidayPeriodJpaEntitiesByEndAt(endAt)
+            .mapNonNull(holidayPeriodMapper::toDomain)
+    }
+
+}
