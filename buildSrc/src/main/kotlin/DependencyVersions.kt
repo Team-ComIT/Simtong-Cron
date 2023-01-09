945cf424ea08514dba3b1910cdfd579b34e4c6c2
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-01-09 20:08:03 +0900

    revert: (#13) MapStruct Dependency Version 제거

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -1,6 +1,5 @@
 object DependencyVersions {
     const val SERVLET = "4.0.1"
-    const val MAPSTRUCT_VERSION = "1.5.2.Final"
     const val SPRING_TRANSACTION = "5.3.22"
     const val QUERYDSL = "5.0.0"
     const val OPEN_FEIGN_VERSION = "3.1.5"
